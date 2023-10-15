9f49d6b56e00883b166cef81161aa378edf9c78d
shin-tc-10 <shin.nkt.09@gmail.com>
2023-10-15 18:32:36 +0900

    css修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,50 @@
 "use client"
 
-import { useState } from 'react';
+import { useState, ChangeEvent, FormEvent } from 'react';
+
+interface Photo {
+  id: number;
+  pageURL: string;
+  largeImageURL: string;
+  tags: string;
+}
 
 export default function Home() {
-  const [searchKeyword, setSearchKeyword] = useState();
-  const [searchResult, setSearchResult] = useState([]);
+  const [searchKeyword, setSearchKeyword] = useState<string>('');
+  const [searchResult, setSearchResult] = useState<Photo[]>([]);
 
-  // 検索キーワードをセット
-  const handleSearchKeyword = (e: any) => {
-    e.preventDefault();
+  const handleSearchKeyword = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchKeyword(e.target.value);
   }
 
-  // 画像を取得する
-  const handleSubmit = (e: any) => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    Search();
-    async function Search() {
-      const response = await fetch(`https://pixabay.com/api/?key=${process.env.NEXT_PUBLIC_PIXABAY_API}&q=${searchKeyword}&image_type=photo`)
-      const photoSearchResult = await response.json();
-      setSearchResult(photoSearchResult.hits);
-      console.log(photoSearchResult);
+    await search();
+  }
 
-    }
+  const search = async () => {
+    const response = await fetch(`https://pixabay.com/api/?key=${process.env.NEXT_PUBLIC_PIXABAY_API}&q=${searchKeyword}&image_type=photo`);
+    const photoSearchResult = await response.json();
+    setSearchResult(photoSearchResult.hits);
   }
 
   return (
-    <div className="photo-search-area p-8">
-      <div className="photo-search-form mb-4">
-        <form onSubmit={handleSubmit} className="flex items-center">
-          <h1 className="text-xl font-bold mr-4">画像を検索</h1>
-          <input
-            type="text"
-            placeholder="キーワードを入力"
-            onChange={handleSearchKeyword}
-            className="border border-gray-300 px-2 py-1 rounded"
-          />
-          <button type="submit" className="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded">
-            検索
-          </button>
-        </form>
+    <div className="photo-search-area p-4 md:p-8">
+      <div className="photo-search-form mb-4 flex flex-col md:flex-row items-center">
+        <h1 className="text-xl font-bold mb-2 md:mr-4">画像を検索</h1>
+        <input
+          type="text"
+          placeholder="キーワードを入力"
+          value={searchKeyword}
+          onChange={handleSearchKeyword}
+          className="border border-gray-300 px-2 py-1 rounded mb-2 md:mb-0 md:mr-2"
+        />
+        <button type="submit" onClick={handleSubmit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded">
+          検索
+        </button>
       </div>
       <div className="photo-disp grid grid-cols-1 md:grid-cols-3 gap-4">
-        {searchResult.map((result: any) => (
+        {searchResult.map((result) => (
           <div key={result.id} className="photo">
             <a href={result.pageURL} target="_blank" rel="noopener noreferrer">
               <img src={result.largeImageURL} alt={result.tags} className="w-full h-auto" />
