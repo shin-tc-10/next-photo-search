9945e91d251942fd1e0d846fcf52b355f710e6bf
shin-tc-10 <shin.nkt.09@gmail.com>
2023-10-19 12:16:46 +0900

    非同期処理をリファクタリング、画像に影を付与

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,12 +17,8 @@ export default function Home() {
     setSearchKeyword(e.target.value);
   }
 
-  const handleSubmit = (e: any) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
-    search();
-  }
-
-  const search = async () => {
     const response = await fetch(`https://pixabay.com/api/?key=${process.env.NEXT_PUBLIC_PIXABAY_API}&q=${searchKeyword}&image_type=photo`);
     const photoSearchResult = await response.json();
     setSearchResult(photoSearchResult.hits);
@@ -32,16 +28,18 @@ export default function Home() {
     <div className="photo-search-area p-4 md:p-8">
       <div className="photo-search-form mb-4 flex flex-col md:flex-row items-center">
         <h1 className="text-xl font-bold mb-2 md:mr-4">画像を検索</h1>
-        <input
-          type="text"
-          placeholder="キーワードを入力"
-          value={searchKeyword}
-          onChange={handleSearchKeyword}
-          className="border border-gray-300 px-2 py-1 rounded mb-2 md:mb-0 md:mr-2"
-        />
-        <button type="submit" onClick={handleSubmit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded">
-          検索
-        </button>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="キーワードを入力"
+            value={searchKeyword}
+            onChange={handleSearchKeyword}
+            className="border border-gray-300 px-2 py-1 rounded mb-2 md:mb-0 md:mr-2"
+          />
+          <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded">
+            検索
+          </button>
+        </form>
       </div>
       <div className="photo-disp grid grid-cols-1 md:grid-cols-3 gap-4">
         {searchResult.map((result) => (
